f0759201601f86e16e0fc3586911befe4b0b7954
suvodeep4119 <suvodeep4119@gmail.com>
2020-02-16 19:01:35 +0530

    ADDED: Feature to reset board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,3 +1,17 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome to the TIC-TAC-TOE Simulator"
+
+#Array
+board=(. . . . . . . . .)
+
+#Functions
+function printBoard()
+{
+	echo "r/c 0 1 2"
+	echo "0 ${board[0]} ${board[1]} ${board[2]}"
+	echo "1 ${board[3]} ${board[4]} ${board[5]}"
+	echo "2 ${board[6]} ${board[7]} ${board[8]}"
+}
+
+printBoard
 
